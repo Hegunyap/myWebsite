a5caed44cc4220f86c684e433181389d6663e6eb
Hendra Gunawan <Hendra.gunawan@webimp.com>
2019-05-05 18:50:20 +0700

    learning plugin and create schoolpress plugin

---
+++ wp-content/plugins/my-plugin/my-plugin.php
@@ -0,0 +1,18 @@
+<?php
+/*
+* Plugin Name: My Plugin
+* Plugin URI: http://webdevstudios.com/
+* Description: Plugin pertama saya.
+* Author: Hendra Gunawan
+* Version: 1.0.0
+* Author URI: http://bwawwp.com
+* License: GPLv2 or later
+*/
+
+function my_plugin_wp_footer() {
+    echo 'I read Building Web Apps with WordPress
+    and now I am a WordPress Genius!';
+}
+
+add_action( 'wp_footer', 'my_plugin_wp_footer' );
+?>
