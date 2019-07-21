05fee6125a4429783311a3c9092ed6e8938a7338
Hendra Gunawan <Hendra.gunawan@webimp.com>
2019-07-21 17:58:27 +0700

    Disable footer plugin

--- wp-content/plugins/my-plugin/my-plugin.php
+++ wp-content/plugins/my-plugin/my-plugin.php
@@ -2,7 +2,7 @@
 /*
 * Plugin Name: My Plugin
 * Plugin URI: http://webdevstudios.com/
-* Description: Plugin pertama saya.
+* Description: Plugin pertama.
 * Author: Hendra Gunawan
 * Version: 1.0.0
 * Author URI: http://bwawwp.com
@@ -11,8 +11,9 @@
 
 function my_plugin_wp_footer() {
     echo 'I read Building Web Apps with WordPress
-    and now I am a WordPress Genius!';
+    and now I am a WordPress Genius! ';
 }
+
 
 add_action( 'wp_footer', 'my_plugin_wp_footer' );
 ?>
