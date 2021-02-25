fa7d1d9dcab29be6267618b69829dbd9b2ab31f4
Hegun <hendra.yap@gmail.com>
2021-02-25 11:45:31 +0700

    wp update

--- wp-content/themes/mythemes/footer.php
+++ wp-content/themes/mythemes/footer.php
@@ -1,7 +1,7 @@
 </div><!-- /.container -->
 
     <footer class="blog-footer">
-      <p>Blog template built for <a href="http://getbootstrap.com">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+      <p>&#169; by Hegun</p>
       <p>
         <a href="#">Back to top</a>
       </p>
